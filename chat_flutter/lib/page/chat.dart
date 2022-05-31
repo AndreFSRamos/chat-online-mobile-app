fc078fcad1f7927fd9ff026d156af5b85a82d2a8
André Felipe da Silva Ramos <andre.sis.tec@hotmail.com>
2022-05-31 17:56:09 -0300

    31/05/2022 - tarde
    - implementando a visualização dos itens do banco.

--- chat_flutter/lib/page/chat.dart
+++ chat_flutter/lib/page/chat.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 import 'package:chat_flutter/page/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -12,26 +13,58 @@ class ChatScreen extends StatefulWidget {
   State<ChatScreen> createState() => _ChatScreenState();
 }
 
-void sendMassage({String? text, File? imgFile}) async {
-  Map<String, dynamic> data = {};
+class _ChatScreenState extends State<ChatScreen> {
+  var _currentuser;
 
-  if (imgFile != null) {
-    UploadTask task = FirebaseStorage.instance
-        .ref()
-        .child(TimeOfDay.now().minute.toString())
-        .putFile(imgFile);
-
-    TaskSnapshot taskSnapshot = await task.whenComplete(() => null);
-    String url = await taskSnapshot.ref.getDownloadURL();
-    data['imgUrl'] = url;
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      _currentuser = user;
+    });
   }
 
-  if (text != null) data['text'] = text;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
 
-  FirebaseFirestore.instance.collection("messagers").add(data);
-}
+  Future _getUser() async {
+    if (_currentuser != null) return _currentuser;
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+      final UserCredential authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
 
-class _ChatScreenState extends State<ChatScreen> {
+      final user = authResult.user;
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  void _sendMassage({String? text, File? imgFile}) async {
+    Map<String, dynamic> data = {};
+
+    if (imgFile != null) {
+      UploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(TimeOfDay.now().minute.toString())
+          .putFile(imgFile);
+
+      TaskSnapshot taskSnapshot = await task.whenComplete(() => null);
+      String url = await taskSnapshot.ref.getDownloadURL();
+      data['imgUrl'] = url;
+    }
+
+    if (text != null) data['text'] = text;
+
+    FirebaseFirestore.instance.collection("messagers").add(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +72,35 @@ class _ChatScreenState extends State<ChatScreen> {
         title: const Text("Olá"),
         elevation: 0,
       ),
-      body: const TextComposer(sendMassager: sendMassage),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("messagers")
+                  .snapshots(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return const CircularProgressIndicator();
+                  default:
+                    List<DocumentSnapshot> documents =
+                        snapshot.data!.docs.reversed.toList();
+                    return ListView.builder(
+                        itemCount: documents.length,
+                        reverse: true,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                              title: Text(documents[index].data['text']));
+                        });
+                }
+              },
+            ),
+          ),
+          TextComposer(sendMassager: _sendMassage),
+        ],
+      ),
     );
   }
 }
