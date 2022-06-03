b6ecada52f8b4fffacb0bd965605d0ac7e4b3ddb
André Felipe da Silva Ramos <andre.sis.tec@hotmail.com>
2022-06-03 00:13:48 -0300

    03/05/2022
    -- comentado o codigo.

--- chat_flutter/lib/page/chat.dart
+++ chat_flutter/lib/page/chat.dart
@@ -5,7 +5,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
 import 'chat_massage.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -19,9 +18,13 @@ class _ChatScreenState extends State<ChatScreen> {
   final GoogleSignIn googleSignIn = GoogleSignIn();
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   // ignore: prefer_typing_uninitialized_variables
-  var _currentuser;
-  bool _isLoading = false;
 
+  // ignore: prefer_typing_uninitialized_variables
+  var _currentuser; // Variavel privada para armezanar o usuario logado.
+  bool _isLoading = false; // contrla o efeito de carregamento de imagem.
+
+  //Função Init e executada uma unica vez ao inicar a aplicação, ela recupera a
+  //instancia do usuário, para determinar se esta logado ou não.
   @override
   void initState() {
     super.initState();
@@ -32,37 +35,54 @@ class _ChatScreenState extends State<ChatScreen> {
     });
   }
 
+  //Função para logar um usuario com a conta google.
   Future _getUser() async {
+    //verifica se já tem uma instancia de usuaro, caso sim, apenas retorna o usuario já logado.
     if (_currentuser != null) return _currentuser;
     try {
+      //chama o login do google.
       final GoogleSignInAccount? googleSignInAccount =
           await googleSignIn.signIn();
+      //faz a autenticação do usuario google.
       final GoogleSignInAuthentication googleSignInAuthentication =
           await googleSignInAccount!.authentication;
+      //recuperar os token de ID e acesso.
       final AuthCredential credential = GoogleAuthProvider.credential(
           idToken: googleSignInAuthentication.idToken,
           accessToken: googleSignInAuthentication.accessToken);
+      //recebe um usuario autenticado firebase.
       final UserCredential authResult =
           await FirebaseAuth.instance.signInWithCredential(credential);
-
+      // aemazena o usuario autenticado fire base.
       final user = authResult.user;
+      //retorna o usuário.
       return user;
     } catch (error) {
       return null;
     }
   }
 
+  // função para enviar as messagem do usuário par ao banco de dados.
+  // Recebe uma função anonima como parametrô, que recebe como parametró
+  // ou o Texto imformado na messagem ou a imagem do usário.
   void _sendMassage({String? text, File? imgFile}) async {
+    //Recupera a instancia do usuário, caso esteja logando.
     var user = await _getUser();
-    /* if (user == null) {
+
+    //Retorna um erro para o usuário caso não consiga efetuar o login, retorna
+    //no formata de snack bar.
+    if (user == null) {
+      // ignore: deprecated_member_use
       _scaffoldKey.currentState!.showSnackBar(
         const SnackBar(
           content: Text('Não foi possivel fazer o login'),
           backgroundColor: Colors.red,
         ),
       );
-      //print("erro ao logar ");
-    }*/
+    }
+
+    //Armazendo as informações do usuario logado, (Id, Nome e hora do envio).
+    //OBS: HORA NO ENVIO SERVE PARA ORDENAR A LISTA DE MENSSAGEM.
     Map<String, dynamic> data = {
       "uid": user.uid,
       "senderName": user.displayName,
@@ -70,39 +90,58 @@ class _ChatScreenState extends State<ChatScreen> {
       //"senderPhotoUrl": user.photoUrl,
     };
 
+    //Verifica se o usuário enviou uma imagem, caso sim, envia para o banco.
+    //(Storage). Caso não seja uma imagem ira cair na verificação se o usuário
+    // enviou um texto.
     if (imgFile != null) {
       UploadTask task = FirebaseStorage.instance
           .ref()
           .child(user.uid + Duration.microsecondsPerMillisecond.toString())
           .putFile(imgFile);
 
+      //Funação setStat para para exibir uma barra de carregamento, caso o usuario
+      // envie uma imagem.
       setState(() {
         _isLoading = true;
       });
 
+      //Recupera a URL da imagem que foi eviada para o Storage.
       TaskSnapshot taskSnapshot = await task.whenComplete(() => null);
       String url = await taskSnapshot.ref.getDownloadURL();
       data['imgUrl'] = url;
 
+      //Finaliza a barra de carregamento recebendo (FALSE), quando terminar de
+      //enviar a imagem para o banco e recupera a URL.
       setState(() {
         _isLoading = false;
       });
     }
-    //print("$text antes de mandar para o banco");
+
+    // Verifica se o usuario enviou um TEXTO.
     if (text != null) data['text'] = text;
 
+    //Enviando o Texto para a coloeção "messagers" no firebase.
     FirebaseFirestore.instance.collection('messagers').add(data);
   }
 
+  //Inicio do layout da aplicação.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
+      //I============ Inicio da AppBar =====================
       appBar: AppBar(
+        //Texto da AppBar, se o usuario estiver logado, ira contanenar "Ola, " +
+        //o nome do usuario, caso contrario exibirar "Chat App"
         title: Text(_currentuser != null
             ? 'Olá, ${_currentuser.displayName}'
             : 'Chat App'),
         elevation: 0,
+        //Action da AppBar, aqui é verificado de o usuario está logado, caso SIM
+        //ira exibir o botão de logout. Caso contrario, a actions retorna um
+        //container vazio, para não exibir nada.
+        //Ao clicar no botão de deslogar, é exibida uma SnackBar informando o
+        //usuário.
         actions: [
           _currentuser != null
               ? IconButton(
@@ -118,27 +157,41 @@ class _ChatScreenState extends State<ChatScreen> {
               : Container()
         ],
       ),
+      //=================== Inicio do Corpo da aplicação ======================
       body: Column(
         children: [
           Expanded(
+            //É ultilizado o StreamBuilder para exibir a lista de messages, salvas
+            //no banco de dados.
             child: StreamBuilder<QuerySnapshot>(
+              //Recuperando a instancia da coleção que será usada, ordenada por
+              //pela hora de envio.
               stream: FirebaseFirestore.instance
                   .collection('messagers')
                   .orderBy('time')
                   .snapshots(),
               builder: (context, snapshot) {
                 switch (snapshot.connectionState) {
+                  //Case 1 verifica se tem conexão, o Case 2 verifica se está
+                  //aguardando uma resposta da conexão, ambas retornando um
+                  //widget CircularProgress para inciar a espera para o usuario.
                   case ConnectionState.none:
                   case ConnectionState.waiting:
                     return const CircularProgressIndicator();
                   default:
+                    //Recuperando informarções da coleção e armazenando um uma List.
                     List<DocumentSnapshot> documents =
                         snapshot.data!.docs.toList();
                     return ListView.builder(
-                      itemCount: documents.length,
+                      itemCount: documents
+                          .length, //recebendo o tamanho da lista como count.
                       reverse: // controla se a conversa começa de cima para baixo ou vise versa.
                           true,
                       itemBuilder: (context, index) {
+                        //Chamando o widget ChatMassage para cara item da lista,
+                        //e passando por parametro as informações do item, e se
+                        //é TRUE ou FALSE que o usuario que mandou a messagem é
+                        //o mesmo que está logado.
                         return ChatMassage(
                             data: documents[index].data(),
                             mine: documents[index]['uid'] == _currentuser?.uid);
@@ -148,6 +201,9 @@ class _ChatScreenState extends State<ChatScreen> {
               },
             ),
           ),
+          //mostra a barra de carregamento do envio da imagem para a variavel
+          //_isLoading seja TRUE, caso contrario, retorna um container vazio,
+          //para não exibir nada.
           _isLoading ? const LinearProgressIndicator() : Container(),
           TextComposer(sendMassager: _sendMassage),
         ],
--- chat_flutter/lib/page/text_composer.dart
+++ chat_flutter/lib/page/text_composer.dart
@@ -5,15 +5,25 @@ import 'package:image_picker/image_picker.dart';
 class TextComposer extends StatefulWidget {
   const TextComposer({Key? key, required this.sendMassager}) : super(key: key);
 
+  //Função enviada pela classe Chat, que recebe por parametro outra função anonima
+  //que tem como parametro uma Strig(Texto enviado do usuario) e uma File(Imagem
+  // enviado pelo usuario), ambas podem ser nulas.
   final Function({String? text, File? imgFile}) sendMassager;
 
   @override
   State<TextComposer> createState() => _TextComposerState();
 }
 
+//variavel _isComposing, auxiia no controle de habilitar e desabilitar o botão
+// de enviar menssagem.
 bool _isComposing = false;
+
+//Controller do campo de texto.
 final TextEditingController controller = TextEditingController();
 
+//Essa classe se trata ta barra de menssagem, que é formado por um container,
+//que possui uma linha, e dentro da linha há um iconButton(Camera), TextFiel(campo de texto)
+//é o botão de enviar menssagem.
 class _TextComposerState extends State<TextComposer> {
   @override
   Widget build(BuildContext context) {
@@ -22,6 +32,10 @@ class _TextComposerState extends State<TextComposer> {
       child: Row(
         children: [
           IconButton(
+            //Função do iconButtom (Camera), ao clicar é aberta a camera do celular
+            //onde pode se tirar a foto ou não. Caso sim pe armazenada a foto e
+            //chamada a função senMassger para retornar a imagem. Caso contrario
+            //cai do IF que valida se a variavel e igual a nula e retorna nada.
             onPressed: () async {
               ImagePicker imagePicker = ImagePicker();
               XFile? imgXFile =
@@ -37,11 +51,19 @@ class _TextComposerState extends State<TextComposer> {
               controller: controller,
               decoration: const InputDecoration.collapsed(
                   hintText: "Enviar uma messagem"),
+              //função onChange do texxField, troca a varial _isComposing
+              // para nulo, para apagar a messagem enviada pelo usuario do campo
+              //necessario o setStat para atualizar a tela em tempo real.
               onChanged: (text) {
                 setState(() {
                   _isComposing = text.isNotEmpty;
                 });
               },
+              //função onSubmitted do texxField, chama a função sendMassager e
+              //passa por parametro o text digitado pelo usuario, em seguida
+              // chama a funação _reset para trocar  a varial _isComposing para
+              //vazio, para desabilitar o botaõ assim que envia a messagem.
+              //nessario o setStat para atualizar a tela em tempo real.
               onSubmitted: (text) {
                 widget.sendMassager(text: text);
                 _reset();
@@ -61,6 +83,7 @@ class _TextComposerState extends State<TextComposer> {
     );
   }
 
+  //função pra auxilar do estado do botão de eviar messagem.
   void _reset() {
     controller.clear();
     setState(() {
